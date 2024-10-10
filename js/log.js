9badaa309e2ca0b85a24ed3a3fdc14e5c907e3cc
ahmet-alsalabi <ahmetalsalabi@gmail.com>
2024-10-11 01:40:20 +0300

    Simple files have been created but some modifications need to be made to them in the future.

---
+++ js/log.js
@@ -0,0 +1,4 @@
+function save() {
+    const custo = document.getElementById("custo").value;
+    alert("Thank you " + custo + " for filling in the blanks.");
+}
---
+++ js/main.js
@@ -0,0 +1,11 @@
+function save() {
+    const custo = document.getElementById ("custo").value;
+    alert("Thank you " + custo + " for filling in the blanks.");
+    const name = document.getElementById("custo").value;
+    const phone = document.getElementById("phone").value;
+    const mess = document.getElementById("messa").value;
+    console.log(
+        "Name: ", name,
+        "Phone: ", phone,
+        "Message: ", mess)
+}
